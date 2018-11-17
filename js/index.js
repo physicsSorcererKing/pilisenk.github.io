69a85898364326303e1a100e20947c324399145e
Shuo Wang <ex76823@gmail.com>
2018-11-17 19:28:55 +0800

    v1.1.3 change bg and navbar style

--- js/index.js
+++ js/index.js
@@ -9,18 +9,31 @@
 		navbarContent.toggleClass('psk-nav-show');
 	}
 
-	function setNavbarTogglerColor() {
-		navbarContent.hasClass('psk-nav-show') ?
-			navbarToggler.css('background-color', 'var(--dark, dark)') :
-			navbarToggler.css('background-color', 'var(--light, white)');
+	function removeNavbarSlide() {
+		
+		navbarContent.collapse('hide');
+		navbarContent.removeClass('psk-nav-show');
 	}
 
+	function setNavbarTogglerColor() {
+		if (navbarContent.hasClass('psk-nav-show')){
+			navbarToggler.css('background-color', 'var(--light, white)');
+			navbarToggler.css('background-image', `url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0, 0, 0, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E")`);
+		}
+		else {
+			navbarToggler.css('background-color', 'var(--dark, dark)');
+			navbarToggler.css('background-image', `url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E")`);
+		}
+	}
 
 	//========================Add event listener========================
 	//set interval time to forbid the status 'psk-nav-show' is not ready.
 	navbarToggler.click(setInterval.bind(this, setNavbarTogglerColor, 100));
-	navbarToggler.blur(setInterval.bind(this, setNavbarTogglerColor, 100));
 	navbarToggler.focus(setInterval.bind(this, setNavbarTogglerColor, 100));
+	navbarToggler.blur(setInterval.bind(this, setNavbarTogglerColor, 100));
+
+	navbarToggler.blur(removeNavbarSlide);
+
 
 	navbarContent.on('show.bs.collapse', toggleNavbarSlide);
 	navbarContent.on('hide.bs.collapse', toggleNavbarSlide);
